f70e8a3bdcd911c11a9fa0eff00ccf02034194d8
Akshit Khatri <akshitkhatri.20@gmail.com>
2025-02-12 20:07:04 +0530

    added TC analysis

--- src/com/practice/leetcode/daily/feb25/MaxSumPairEqualDigits2342_12.java
+++ src/com/practice/leetcode/daily/feb25/MaxSumPairEqualDigits2342_12.java
@@ -3,6 +3,20 @@ package com.practice.leetcode.daily.feb25;
 import java.util.PriorityQueue;
 
 public class MaxSumPairEqualDigits2342_12 {
+    /*
+    * Time Complexity: O(nlogm)
+    The time complexity of this approach is primarily determined by the operations performed
+    on the input array nums and the computation of digit sums. The calculateDigitSum function
+    computes the sum of digits for a given number, which takes O(logm) time.
+    This is because the number of digits in a number is proportional to log m.
+    The first loop iterates over all n elements in nums and computes their digit sums, resulting in a total time of O(nlogm).
+    The second loop also iterates over all n elements in nums. For each element, it performs a push operation on a priority queue (min-heap).
+    Since the heap size is limited to 2, each push operation takes O(1) time. Thus, this loop contributes O(n) to the time complexity.
+    Finally, the third loop iterates over the digitSumGroups array, which has a size proportional to the maximum digit sum, O(logm).
+    For each heap of size 2, it performs two pop operations and a sum computation, each taking O(1) time. This loop adds O(logm) to the time complexity.
+    Combining all these, the overall time complexity is O(nlogm).
+    *
+    * */
     public int maximumSum(int[] nums) {
         int ans = Integer.MIN_VALUE;
         PriorityQueue<Integer>[] pq = new PriorityQueue[82];
