f219317a00c70cbaf79e4b87babcbab05ed61f24
Akshit Khatri <akshitkhatri.20@gmail.com>
2025-04-03 09:04:05 +0530

    added maximum triplet value

---
+++ src/com/practice/leetcode/daily/april25/MaximumTripletValue2873_2.java
@@ -0,0 +1,16 @@
+package com.practice.leetcode.daily.april25;
+
+public class MaximumTripletValue2873_2 {
+    public long maximumTripletValue(int[] nums) {
+        long ans = 0;
+        int n = nums.length;
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = i+1 ; j < n ; j++) {
+                for (int k = j+1 ; k < n ; k++) {
+                    ans = Math.max(ans, ((long)(nums[i]-nums[j]) * nums[k]));
+                }
+            }
+        }
+        return ans;
+    }
+}
