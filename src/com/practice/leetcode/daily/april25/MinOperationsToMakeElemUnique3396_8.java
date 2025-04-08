c18a28ff55fe30b2991a87686503c08951e2201d
Akshit Khatri <akshitkhatri.20@gmail.com>
2025-04-08 19:38:18 +0530

    added min operations

---
+++ src/com/practice/leetcode/daily/april25/MinOperationsToMakeElemUnique3396_8.java
@@ -0,0 +1,26 @@
+package com.practice.leetcode.daily.april25;
+
+import java.util.HashSet;
+
+public class MinOperationsToMakeElemUnique3396_8 {
+    public int minimumOperations(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i += 3, ans++) {
+            if (checkUnique(nums, i)) {
+                return ans;
+            }
+        }
+        return ans;
+    }
+
+    private boolean checkUnique(int[] nums, int start) {
+        HashSet<Integer> cnt = new HashSet<>();
+        for (int i = start; i < nums.length; i++) {
+            if (cnt.contains(nums[i])) {
+                return false;
+            }
+            cnt.add(nums[i]);
+        }
+        return true;
+    }
+}
