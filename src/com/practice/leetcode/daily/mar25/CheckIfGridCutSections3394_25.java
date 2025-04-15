80186a4cf64dba3a27908eb233358398b08f874e
Akshit Khatri <akshitkhatri.20@gmail.com>
2025-04-15 19:25:03 +0530

    added check if grid exists

---
+++ src/com/practice/leetcode/daily/mar25/CheckIfGridCutSections3394_25.java
@@ -0,0 +1,17 @@
+package com.practice.leetcode.daily.mar25;
+
+public class CheckIfGridCutSections3394_25 {
+    /**
+     * Interview Tip: In-Place Algorithms
+     * In-place algorithms overwrite the input to save space, but sometimes this can cause problems.
+     * Here are a couple of situations where an in-place algorithm might not be suitable:
+     *
+     * The algorithm needs to run in a multi-threaded environment without exclusive access to the array.
+     * Other threads might need to read the array as well and may not expect it to be modified.
+     * Even if there is only a single thread or the algorithm has exclusive access to the array while running,
+     * the array might need to be reused later or by another thread once the lock has been released.
+     * In an interview, always check whether the interviewer is okay with you overwriting the input.
+     * Be prepared to explain the pros and cons of doing so if asked!
+     */
+
+}
