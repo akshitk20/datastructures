377d510fefc45b7d5b61303c6462f00aed1e2618
Akshit Khatri <akshitkhatri.20@gmail.com>
2025-04-21 10:04:21 +0530

    added count hidden sequence

---
+++ src/com/practice/leetcode/daily/april25/CountHiddenSequence2145_21.java
@@ -0,0 +1,15 @@
+package com.practice.leetcode.daily.april25;
+
+public class CountHiddenSequence2145_21 {
+
+    public int numberOfArrays(int[] differences, int lower, int upper) {
+        long min = 0, max = 0, sum = 0;
+        for (int n : differences) {
+            sum += n;
+            max = Math.max(max, sum);
+            min = Math.min(min, sum);
+        }
+
+        return (int) Math.max(0, (upper-max) - (lower-min) + 1);
+    }
+}
