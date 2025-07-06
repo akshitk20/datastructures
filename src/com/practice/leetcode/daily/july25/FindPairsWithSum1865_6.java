5a3f4dded6e37ae9ea87a4a200330afc69748658
Akshit Khatri <akshitkhatri.20@gmail.com>
2025-07-06 09:44:56 +0530

    added find pairs with sum

---
+++ src/com/practice/leetcode/daily/july25/FindPairsWithSum1865_6.java
@@ -0,0 +1,35 @@
+package com.practice.leetcode.daily.july25;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindPairsWithSum1865_6 {
+    private int[] nums1;
+    private int[] nums2;
+    private Map<Integer, Integer> cnt;
+
+    public FindPairsWithSum1865_6(int[] nums1, int[] nums2) {
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        this.cnt = new HashMap<>();
+        for (int num : nums2) {
+            cnt.put(num, cnt.getOrDefault(num, 0) + 1);
+        }
+    }
+
+    public void add(int index, int val) {
+        int oldVal = nums2[index];
+        cnt.put(oldVal, cnt.get(oldVal) - 1);
+        nums2[index] += val;
+        cnt.put(nums2[index], cnt.getOrDefault(nums2[index], 0) + 1);
+    }
+
+    public int count(int tot) {
+        int ans = 0;
+        for (int num : nums1) {
+            int rest = tot - num;
+            ans += cnt.getOrDefault(rest, 0);
+        }
+        return ans;
+    }
+}
